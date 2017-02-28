7a658769bd1f8eca9818feab7989af4051ab2f04
asxalex <asxalex@163.com>
2017-02-28 22:10:53 +0800

    gcode_runner

--- gcode_runner.py
+++ gcode_runner.py
@@ -71,6 +71,15 @@ class Command(object):
         if x1 == x0 and y1 == y0:
             return x1, y1, z1
 
+        if x1 == x0:
+            min_y = int(round(min(y0, y1)))
+            max_y = int(round(max(y0, y1)))
+            x = round(x0)
+            for y in range(min_y, max_y + 1):
+                img[(x, y)] = 0
+                print (x, y), " is colored"
+            return x1, y1, z1
+
         k = 1.0 * (y1 - y0) / (x1 - x0)
         b = 1.0 * (x1*y0 - x0*y1) / (x1-x0)
 
@@ -121,7 +130,7 @@ class Runner(object):
         self.currenty = 0
         self.currentz = 0
 
-    def run_code(self, filename):
+    def run_code(self, filename, outname):
         try:
             fp = open(filename)
         except Exception, e:
@@ -139,11 +148,31 @@ class Runner(object):
             command = Command(line, currentx, currenty, currentz)
             currentx, currenty, currentz = command.action(self.img_array)
 
-        self.img.save("test.png", "PNG")
+        self.img.save(outname, "PNG")
+
+def get_max(filename):
+    max_x = 0
+    max_y = 0
+    for line in open(filename):
+        snippets = line.split()
+        for i in snippets:
+            if i.lower().startswith("x"):
+                x = float(i[1:])
+                if max_x < x:
+                    max_x = x
+                    continue
+            if i.lower().startswith("y"):
+                y = float(i[1:])
+                if max_y < y:
+                    max_y = y
+                    continue
+    max_x = int(max_x * 1.1)
+    max_y = int(max_y * 1.1)
+    return max_x, max_y
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "usage: python2 gcode_runner.py [width] [height]"
+        print "usage: python2 gcode_runner.py inputfile [outfile] [width] [height]"
         sys.exit(-1)
 
     x = 500
@@ -152,10 +181,17 @@ if __name__ == "__main__":
         filename = sys.argv[1]
 
     if len(sys.argv) >= 3:
-        x = int(sys.argv[2])
-    if len(sys.argv) >=4:
-        y = int(sys.argv[3])
+        outfile = sys.argv[2]
+    else:
+        outfile = "test.png"
+
+    x, y = get_max(filename)
+
+    if len(sys.argv) >= 4:
+        x = int(sys.argv[3])
+    if len(sys.argv) >= 5:
+        y = int(sys.argv[4])
 
     runner = Runner(x, y)
-    runner.run_code(filename)
+    runner.run_code(filename, outfile)
 
